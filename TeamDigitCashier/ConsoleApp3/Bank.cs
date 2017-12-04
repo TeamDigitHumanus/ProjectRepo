6e087cfc77c2746ffd07d9dc04e4367dbdf365af
FinnSven <jamie.hussey@gmail.com>
2017-12-04 10:17:33 +0100

    Add files via upload
    Lets see if this works

---
+++ TeamDigitCashier/ConsoleApp3/Bank.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamDigitCashier
+{
+    class Bank
+    {
+        int cUstomerNo;
+        string name;
+        int personNumber;
+             
+        int withDraw;
+        
+    }
+    
+}
---
+++ TeamDigitCashier/ConsoleApp3/ReceiptSystem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamDigitCashier
+{
+    class RecieptSystem 
+    {
+        string companyName;
+        string terminalOperator;
+        int organnumber;
+        int producTtotal;
+        int subTotal;
+        int Vat;
+        DateTime
+        string Print;
+        Console.WriteLine()
+        
+    }
+}
